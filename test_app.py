d7938189bcd5a8c0666a80c7329d3f79d93bdba3
HCBrooks-lab <h.brooks29@icloud.com>
2025-06-26 14:37:10 -0500

    Added updated file
    Added update

--- test_app.py
+++ test_app.py
@@ -4,3 +4,19 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
+
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0.0) == 0
+
+def test_add_large_numbers():
+    assert add(1000, 2000) == 3000
+
+def test_add_negative_numbers():
+    assert add(-5, -5) == -10
+
+
